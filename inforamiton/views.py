40ccb033c0ea2af8e9c1d3068c04e5521fa2e46a
thearchduke <tynanburke@gmail.com>
2013-02-04 13:43:06 -0800

    Added weather to /i/

--- inforamiton/views.py
+++ inforamiton/views.py
@@ -1,5 +1,5 @@
 from flask import render_template
-from inforamiton import app
+from inforamiton import app, weather
 
 @app.route('/', methods=['GET'])
 def home():
@@ -7,4 +7,5 @@ def home():
 
 @app.route('/i/<lat>&<lon>', methods=['GET'])
 def info(lat,lon):
-	return render_template('info.html', lat=lat, lon=lon)
+	temp, place = weather.weather_from_gps(lat, lon)
+	return render_template('info.html', lat=lat, lon=lon, temp=temp, place=place)
