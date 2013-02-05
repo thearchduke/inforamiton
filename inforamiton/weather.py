c0dda10c9e5d9025aa7f7d0d6f7edf286d7018ff
thearchduke <tynanburke@gmail.com>
2013-02-05 08:49:54 -0800

    Fixed r.json error

--- inforamiton/weather.py
+++ inforamiton/weather.py
@@ -8,11 +8,12 @@ import random
 def weather_from_gps(lat, lon):
 	payload = 'http://api.wunderground.com/api/' + config['wunderkey'] + '/geolookup/q/' + str(lat) + ',' + str(lon) + '.json'
 	r = requests.get(payload)
-	foo = r.json()
+	print r
+	foo = r.json
 	place = foo['location']['city'] + ', ' + foo['location']['state']
 	payload = 'http://api.wunderground.com/api/ae007812a56622b8/conditions/q/' + foo['location']['state'] + '/' + foo['location']['city'] + '.json'
 	r = requests.get(payload)
-	weather = r.json()
+	weather = r.json
 	return weather['current_observation']['temp_f'], place
 
 
